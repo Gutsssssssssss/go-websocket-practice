27e066177da746973e372a4bc046ce6fba1627b7
imjm <junsk50@naver.com>
2025-07-01 19:36:22 +0900

    connect ws

---
+++ client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type Client struct {
+	hub *Hub
+
+	// The websocket connection
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages
+	send chan []byte
+}
+
+// serveWs handles websocket request from the peer
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+}
